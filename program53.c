4c6576d3ac19b47334170ee5a4df9f07371986eb
Sujeet2204 <sujeetlamjane@gmail.com>
2026-07-27 22:21:55 +0530

    Add files via upload

---
+++ program53.c
@@ -0,0 +1,41 @@
+//number of factors and non factors both ..
+
+#include<stdio.h>
+
+void CountFactorsNonFactors(int iNo)
+{
+    int iCnt =0, iFrequency1= 0, iFrequency2=0;
+
+    if(iNo<0)           //modifier
+    {
+        iNo=-iNo;
+    }
+
+    for(iCnt=1;iCnt <iNo;iCnt++)
+    {
+        if((iNo % iCnt)==0)
+        {
+            iFrequency1++;
+        }
+        else
+        {
+            iFrequency2++;
+        }
+    }
+
+    printf("number of factors are : %d\n",iFrequency1);
+    printf("number of non factors are : %d\n",iFrequency2);
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+
+    CountFactorsNonFactors(iValue);
+
+    return 0;
+}
